a9636ee267daa17b003e91bae7ed7b746eaf6a45
Matthew Perry <muffinman616@gmail.com>
2015-10-15 11:27:37 -0400

    Configure setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='ktane_py',
+      version='0.1',
+      description='Module solver for Keep Talking and Nobody Explodes',
+      url='https://github.com/mattvperry/ktane_py',
+      author='Matt Perry',
+      author_email='muffinman616@gmail.com',
+      license='MIT',
+      packages=['ktane'],
+      scripts=['bin/ktane'],
+      zip_safe=False)
+
